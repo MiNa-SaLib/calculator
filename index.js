39b63f080428556882c6817a777bea73438621f8
MiNa SaLib <99540302+MiNa-SaLib@users.noreply.github.com>
2022-07-13 19:36:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+let flag = 0;
+let beforeval;
 let bc = document.getElementById("buc");
 let b1 = document.getElementById("bu1");
 let b2 = document.getElementById("bu2");
@@ -18,17 +20,15 @@ let mod = document.getElementById("mod");
 let ans = document.getElementById("ans");
 
 let display = document.getElementById("display");
-function inser(m) {
-  console.log(display.textContent[0]);
-  display.innerHTML += m.textContent;
-}
+
 function back() {
   display.innerHTML = display.textContent.slice(0, length - 1);
 }
+
 function DEL() {
-  ans = +display.textContent;
   display.innerHTML = "";
 }
+
 function equal() {
   for (let i = 0; i < display.innerHTML.length; i++) {
     if (display.textContent[i] == display.textContent[i + 1]) {
@@ -55,12 +55,45 @@ function equal() {
       }
     }
   }
-  if (display.textContent[0] == "." && display.textContent[1] == ".")
+  if (display.textContent[0] == "." && display.textContent[1] == ".") {
     display.innerHTML = `Erorr`;
-  if (display.textContent[0] == "*" || display.textContent[0] == "/")
+  }
+  if (display.textContent[0] == "*" || display.textContent[0] == "/") {
     display.innerHTML = `Erorr`;
+  }
+
+  beforeval = display.textContent;
+  console.log(beforeval); //
+
   display.innerHTML = eval(display.textContent);
+  flag = 1;
+  console.log(typeof +display.textContent); //console
+
+  ans = +display.textContent;
 }
+
+function inser(m) {
+  console.log(flag);
+  console.log(m.textContent);
+  if (flag == 1) {
+    if (
+      m.textContent == "1" ||
+      m.textContent == "2" ||
+      m.textContent == "3" ||
+      m.textContent == "4" ||
+      m.textContent == "5" ||
+      m.textContent == "6" ||
+      m.textContent == "7" ||
+      m.textContent == "8" ||
+      m.textContent == "9"
+    ) {
+      display.innerHTML = "";
+    }
+  }
+  display.innerHTML += m.textContent;
+  flag = 0;
+}
+
 function getans() {
   if (ans.textContent == "Ans") display.innerHTML = "";
   else display.innerHTML += ans;
